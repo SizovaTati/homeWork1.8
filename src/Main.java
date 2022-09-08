a3989830577789db6db0f67ba0a7a2ee46311ff7
SizovaTati <sizoffa25@gmail.com>
2022-09-08 18:31:14 +0300

    Method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        checkYear(2036);
+        System.out.println();
+
+        printInstallationMessage();
+        System.out.print("\n");
+
+        printDeliveryDays(89);
+
+    }
+
+    private static void checkYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " - високосный год");
+        } else {
+            System.out.println(year + " - не високосный год");
+        }
+    }
+
+
+    private static void printInstallationMessage() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер ОС, где 0 — iOS, а 1 — Android: ");
+        int clientOs = scanner.nextInt();
+
+        if (clientOs != 1 && clientOs != 0) {
+            throw new RuntimeException("Устройство не определено");
+        }
+
+        System.out.println("Введите год выпуска устройства: ");
+        int clientDeviceYear = scanner.nextInt();
+        String versionMessage = clientOs == 0 ? "Ios" : "Android";
+        int currentYear = LocalDate.now().getYear();
+        String productionYear = clientDeviceYear < currentYear ? "облегченную" : "";
+        System.out.printf("Установите %s версию приложения для %s по ссылке", productionYear, versionMessage);
+
+    }
+
+    private static void printDeliveryDays(int deliveryDistance) {
+        int start = 20;
+        int step = 40;
+        int deliveryDays;
+        if (deliveryDistance < start) {
+            deliveryDays = 1;
+        } else {
+            deliveryDays = ((Math.abs(deliveryDistance - start) / step) + 2);
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
     }
 }
